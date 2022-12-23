c94cec78059169700cdbac9db7f064d226b4b86a
Pablo Andres Garcia <garciasjs@gmail.com>
2022-12-23 10:57:47 -0300

    msj

--- js/app.js
+++ js/app.js
@@ -24,6 +24,26 @@ UI.prototype.llenarOpciones = ()=>{
     }
 }
 
+//Muestra alertas en pantalla
+
+UI.prototype.mostrarMensaje = (mensaje, tipo)=> {
+    const div = document.createElement('div')
+    if(tipo === 'error') {
+        div.classList.add('mensaje', 'error');
+    } else {
+        div.classList.add('mensaje', 'correcto');
+    }
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    //Insertar el mensaje HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+    setTimeout(()=>{
+        div.remove();
+    }, 3000)
+}
+
 //INSTANCIAR UI
 
 const ui = new UI();
@@ -33,3 +53,36 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', ()=>{
     ui.llenarOpciones(); //Llena el select con los años
 })
+
+EventListener();
+function addEventListener(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+
+    //Leer marca selecionada
+
+    const marca = document.querySelector('#marca').value;
+
+    
+    //Leer año selecionada
+    const year = document.querySelector('#year').value;
+    
+    //Leer covertura selecionada
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    if(marca==='' || year === '' || tipo === ''){
+        ui.mostrarMensaje('Todos los campos son obligatorios, revisa y vuelve a probar', 'error');
+    }  
+        ui.mostrarMensaje('Cotizando...', 'exito');
+   
+    //Instanciar el seguro
+
+
+    // Utilizar el proto que va a cotizar
+
+
+
+}
