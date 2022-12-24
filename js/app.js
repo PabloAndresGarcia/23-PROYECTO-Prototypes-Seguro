5b89dae951d4b87adc9fa04fdb8833efe2968ab3
Pablo Andres Garcia <garciasjs@gmail.com>
2022-12-24 10:21:45 -0300

    js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,54 @@ function Seguro (marca, year, tipo){
     this.year = year;
     this.tipo = tipo;
 }
+//Realiza la cotizacion con los datos
+Seguro.prototype.cotizarSeguro = function (){
+
+    /*
+        1= Americano 1.15
+        2= Asiatico 1.05
+        3= Europeo 1.35
+    */ 
+
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca){
+        case '1':
+            cantidad = base * 1.15
+            break;
+        case '2':
+            cantidad = base * 1.05
+            break;
+        case '3':
+            cantidad = base * 1.35
+            break;
+        default:
+        break;
+    }
+
+    //Leer el año
+    const diferencia = new Date().getFullYear() - this.year;
+    //Cada año que la diferencia es mayor, el costo va a reducirse un 3%
+    cantidad -= (( diferencia * 3 ) * cantidad ) / 100;
+
+
+
+    /*
+        Si el seguro es base se mult por 30%
+        Si el seguro es comple se mult por 50%
+    */
+
+    if (this.tipo === 'basico'){
+        cantidad *=1.30;
+    } else {
+        cantidad *=1.50;
+    }
+    return cantidad;
+
+}
+
+
 
 function UI (){
 
@@ -79,7 +127,8 @@ function cotizarSeguro(e){
         ui.mostrarMensaje('Cotizando...', 'exito');
    
     //Instanciar el seguro
-
+    const seguro = new seguro (marca, year, tipo);
+    seguro.cotizarSeguro();
 
     // Utilizar el proto que va a cotizar
 
